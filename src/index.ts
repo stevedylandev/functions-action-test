e1692d0744393d77a66510d258a89ef35b6706a2
stevedylandev <stevedsimkins@gmail.com>
2025-06-22 19:52:26 -0400

    chore: Updated hook command

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+app.get("/hello", async (c) => {
+	return c.json({
+		message: "Hello",
+		person: "Steve",
+	});
+});
 
 export default {
-  async fetch(request: Request, env: any, ctx: any) {
-    return app.fetch(request, env, ctx)
-  }
-}
+	async fetch(request: Request, env: any, ctx: any) {
+		return app.fetch(request, env, ctx);
+	},
+};
